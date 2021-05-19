51aa87cccb100d2dd0a2305d327922a0c2995e70
antoine65 <antoine.lcts@gmail.com>
2021-05-19 16:02:50 +0200

    Ajout du logo Gallery Project sur la MainActivity

--- app/src/main/java/com/example/gall/WelcomeScreen.java
+++ app/src/main/java/com/example/gall/WelcomeScreen.java
@@ -3,12 +3,41 @@ package com.example.gall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class WelcomeScreen extends AppCompatActivity {
+    Button Seconnecter;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        Seconnecter = findViewById(R.id.Seconnecter);
+        Seconnecter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(WelcomeScreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
     }
+
+
 }
+
